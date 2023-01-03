4d0744f84e69e870f5b0c5c55acf3eff6ff776b2
Yi Kuang <y24kuang@uwaterloo.ca>
2023-01-03 02:22:06 -0500

    1.7

--- SecurityServiceModule/src/test/java/com/udacity/security/service/SecurityServiceTest.java
+++ SecurityServiceModule/src/test/java/com/udacity/security/service/SecurityServiceTest.java
@@ -43,155 +43,137 @@ public class SecurityServiceTest {
     }
 
 
-    @Test
+    @ParameterizedTest
+    @EnumSource(value = ArmingStatus.class, names = {"ARMED_HOME", "ARMED_AWAY"})
     @DisplayName("Test 1")
-    public void alarm_armed_and_sensors_activated_test() {
+    public void alarm_armed_and_sensors_activated_test(ArmingStatus armingStatus) {
         Mockito.when(sensor1.getActive()).thenReturn(false);
-        securityService.setArmingStatus(ArmingStatus.ARMED_AWAY);
+        Mockito.when(securityRepository.getArmingStatus()).thenReturn(armingStatus);
+        Mockito.when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.NO_ALARM);
         securityService.changeSensorActivationStatus(sensor1, true);
-        Assertions.assertEquals(AlarmStatus.PENDING_ALARM, securityService.getAlarmStatus());
-    }
-
-    @Test
-    public void alarm_armed_and_sensor_activated_pending_alarm_test() {
-        Mockito.when(sensor1.getActive()).thenReturn(false);
-        Mockito.when(sensor2.getActive()).thenReturn(false);
-        securityService.setArmingStatus(ArmingStatus.ARMED_HOME);
-        securityService.changeSensorActivationStatus(sensor1, true);
-        Assertions.assertEquals(AlarmStatus.PENDING_ALARM, securityService.getAlarmStatus());
-        securityService.setArmingStatus(ArmingStatus.ARMED_AWAY);
-        securityService.changeSensorActivationStatus(sensor2, true);
-        Assertions.assertEquals(AlarmStatus.ALARM, securityService.getAlarmStatus());
-    }
-
-    // Not sure if test3 is fine.
-    @RepeatedTest(2)
-    public void pending_alarm_inactive_sensors_test(RepetitionInfo repetitionInfo) {
-        Mockito.when(sensor1.getActive()).thenReturn(true);
-        securityService.setAlarmStatus(AlarmStatus.PENDING_ALARM);
-        if (repetitionInfo.getCurrentRepetition() == 1) {
-            securityService.setArmingStatus(ArmingStatus.ARMED_HOME);
-        } else {
-            securityService.setArmingStatus(ArmingStatus.ARMED_AWAY);
-        }
-        securityService.changeSensorActivationStatus(sensor1, false);
-        Assertions.assertEquals(AlarmStatus.NO_ALARM, securityService.getAlarmStatus());
-    }
-
-    @RepeatedTest(2)
-    public void active_alarm_changing_sensors_test(RepetitionInfo repetitionInfo) {
-        if (repetitionInfo.getCurrentRepetition() == 1) {
-            securityService.setArmingStatus(ArmingStatus.ARMED_HOME);
-        } else {
-            securityService.setArmingStatus(ArmingStatus.ARMED_AWAY);
-        }
-        Mockito.when(sensor1.getActive()).thenReturn(false);
-        Mockito.when(sensor2.getActive()).thenReturn(false);
-        securityService.changeSensorActivationStatus(sensor1, true);
-        Mockito.when(sensor1.getActive()).thenReturn(true);
-        Assertions.assertEquals(AlarmStatus.PENDING_ALARM, securityService.getAlarmStatus());
-        securityService.changeSensorActivationStatus(sensor2, true);
-        Mockito.when(sensor2.getActive()).thenReturn(true);
-        Assertions.assertEquals(AlarmStatus.ALARM, securityService.getAlarmStatus());
-        securityService.changeSensorActivationStatus(sensor1, false);
-        Assertions.assertEquals(AlarmStatus.ALARM, securityService.getAlarmStatus());
-        securityService.changeSensorActivationStatus(sensor2, false);
-        Assertions.assertEquals(AlarmStatus.ALARM, securityService.getAlarmStatus());
-    }
-
-
-    @RepeatedTest(2)
-    public void one_sensor_active_another_being_activated_test(RepetitionInfo repetitionInfo) {
-        if (repetitionInfo.getCurrentRepetition() == 1) {
-            securityService.setArmingStatus(ArmingStatus.ARMED_HOME);
-        } else {
-            securityService.setArmingStatus(ArmingStatus.ARMED_AWAY);
-        }
-        Mockito.when(sensor1.getActive()).thenReturn(false);
-        Mockito.when(sensor2.getActive()).thenReturn(false);
-        securityService.changeSensorActivationStatus(sensor1, true);
-        Assertions.assertEquals(AlarmStatus.PENDING_ALARM, securityService.getAlarmStatus());
-        securityService.changeSensorActivationStatus(sensor2, true);
-        Assertions.assertEquals(AlarmStatus.ALARM, securityService.getAlarmStatus());
+        Mockito.verify(securityRepository).setAlarmStatus(AlarmStatus.PENDING_ALARM);
     }
 
     @ParameterizedTest
-    @EnumSource(AlarmStatus.class)
-    public void sensor_deactivated_already_inactive_test(AlarmStatus alarmStatus) {
-        securityService.setArmingStatus(ArmingStatus.ARMED_AWAY);
-        securityService.setAlarmStatus(alarmStatus);
+    @EnumSource(value = ArmingStatus.class, names = {"ARMED_HOME", "ARMED_AWAY"})
+    @DisplayName("Test 2")
+    public void alarm_armed_and_sensor_activated_pending_alarm_test(ArmingStatus armingStatus) {
         Mockito.when(sensor1.getActive()).thenReturn(false);
+        Mockito.when(securityRepository.getArmingStatus()).thenReturn(armingStatus);
+        Mockito.when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.PENDING_ALARM);
+        securityService.changeSensorActivationStatus(sensor1, true);
+        Mockito.verify(securityRepository).setAlarmStatus(AlarmStatus.ALARM);
+    }
+
+
+    @Test
+    @DisplayName("Test 3")
+    public void pending_alarm_inactive_sensors_test() {
+        Mockito.when(sensor1.getActive()).thenReturn(true);
+        Mockito.when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.PENDING_ALARM);
         securityService.changeSensorActivationStatus(sensor1, false);
-        Assertions.assertEquals(alarmStatus, securityService.getAlarmStatus());
+        Mockito.verify(securityRepository).setAlarmStatus(AlarmStatus.NO_ALARM);
     }
 
     @Test
+    @DisplayName("Test 4")
+    public void active_alarm_changing_sensors_test() {
+        Mockito.when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.ALARM);
+        Mockito.when(sensor1.getActive()).thenReturn(true);
+        Mockito.when(sensor2.getActive()).thenReturn(true);
+        securityService.changeSensorActivationStatus(sensor1, false);
+        Mockito.verify(securityRepository, Mockito.never()).setAlarmStatus(AlarmStatus.PENDING_ALARM);
+        Mockito.verify(securityRepository, Mockito.never()).setAlarmStatus(AlarmStatus.NO_ALARM);
+        securityService.changeSensorActivationStatus(sensor2, false);
+        Mockito.verify(securityRepository, Mockito.never()).setAlarmStatus(AlarmStatus.PENDING_ALARM);
+        Mockito.verify(securityRepository, Mockito.never()).setAlarmStatus(AlarmStatus.NO_ALARM);
+    }
+
+
+    @ParameterizedTest
+    @EnumSource(value = ArmingStatus.class, names = {"ARMED_HOME", "ARMED_AWAY"})
+    @DisplayName("Test 5")
+    public void one_sensor_active_another_being_activated_test(ArmingStatus armingStatus) {
+        Mockito.when(securityRepository.getArmingStatus()).thenReturn(armingStatus);
+        Mockito.when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.NO_ALARM);
+        Mockito.when(sensor1.getActive()).thenReturn(false);
+        Mockito.when(sensor2.getActive()).thenReturn(false);
+        securityService.changeSensorActivationStatus(sensor1, true);
+        Mockito.verify(securityRepository).setAlarmStatus(AlarmStatus.PENDING_ALARM);
+        Mockito.when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.PENDING_ALARM);
+        securityService.changeSensorActivationStatus(sensor2, true);
+        Mockito.verify(securityRepository).setAlarmStatus(AlarmStatus.ALARM);
+    }
+
+    @Test
+    @DisplayName("Test 6")
+    public void sensor_deactivated_already_inactive_test() {
+        Mockito.when(sensor1.getActive()).thenReturn(false);
+        securityService.changeSensorActivationStatus(sensor1, false);
+        Mockito.verify(securityRepository, Mockito.never()).setAlarmStatus(AlarmStatus.ALARM);
+        Mockito.verify(securityRepository, Mockito.never()).setAlarmStatus(AlarmStatus.PENDING_ALARM);
+        Mockito.verify(securityRepository, Mockito.never()).setAlarmStatus(AlarmStatus.NO_ALARM);
+    }
+
+    @Test
+    @DisplayName("Test 7")
     public void contains_cat_and_armed_home_test() {
-        securityService.setArmingStatus(ArmingStatus.ARMED_HOME);
+        Mockito.when(securityRepository.getArmingStatus()).thenReturn(ArmingStatus.ARMED_HOME);
         Mockito.when(imageService.imageContainsCat(bufferedImage, 50.0f)).thenReturn(true);
         securityService.processImage(bufferedImage);
-        Assertions.assertEquals(AlarmStatus.ALARM, securityService.getAlarmStatus());
+        Mockito.verify(securityRepository).setAlarmStatus(AlarmStatus.ALARM);
     }
 
     @RepeatedTest(2)
+    @DisplayName("Test 8")
     public void does_not_contains_cat_test(RepetitionInfo repetitionInfo) {
         Mockito.when(imageService.imageContainsCat(bufferedImage, 50.0f)).thenReturn(false);
         Mockito.when(sensor1.getActive()).thenReturn(false);
         Mockito.when(sensor2.getActive()).thenReturn(false);
-        securityService.setAlarmStatus(AlarmStatus.PENDING_ALARM);
         if (repetitionInfo.getCurrentRepetition() == 1) {
             Mockito.when(sensor3.getActive()).thenReturn(true);
             sensorSet = new HashSet<>(Arrays.asList(sensor1, sensor2, sensor3));
             Mockito.when(securityService.getSensors()).thenReturn(sensorSet);
             securityService.processImage(bufferedImage);
-            Assertions.assertEquals(AlarmStatus.PENDING_ALARM, securityService.getAlarmStatus());
+            Mockito.verify(securityRepository, Mockito.never()).setAlarmStatus(AlarmStatus.ALARM);
+            Mockito.verify(securityRepository, Mockito.never()).setAlarmStatus(AlarmStatus.NO_ALARM);
+            Mockito.verify(securityRepository, Mockito.never()).setAlarmStatus(AlarmStatus.PENDING_ALARM);
         } else {
             Mockito.when(sensor3.getActive()).thenReturn(false);
             sensorSet = new HashSet<>(Arrays.asList(sensor1, sensor2, sensor3));
             Mockito.when(securityService.getSensors()).thenReturn(sensorSet);
             securityService.processImage(bufferedImage);
-            Assertions.assertEquals(AlarmStatus.NO_ALARM, securityService.getAlarmStatus());
+            Mockito.verify(securityRepository).setAlarmStatus(AlarmStatus.NO_ALARM);
         }
     }
 
-    @RepeatedTest(2)
-    public void system_disarmed_test(RepetitionInfo repetitionInfo) {
-        securityService.setAlarmStatus(AlarmStatus.PENDING_ALARM);
-        if (repetitionInfo.getCurrentRepetition() == 1) {
-            securityService.setArmingStatus(ArmingStatus.ARMED_HOME);
-        } else {
-            securityService.setAlarmStatus(AlarmStatus.ALARM);
-            securityService.setArmingStatus(ArmingStatus.ARMED_AWAY);
-        }
+    @Test
+    @DisplayName("Test 9")
+    public void system_disarmed_test() {
         securityService.setArmingStatus(ArmingStatus.DISARMED);
-        Assertions.assertEquals(AlarmStatus.NO_ALARM, securityService.getAlarmStatus());
+        Mockito.verify(securityRepository).setAlarmStatus(AlarmStatus.NO_ALARM);
     }
 
-    @RepeatedTest(2)
-    public void system_armed_test(RepetitionInfo repetitionInfo) {
+    @ParameterizedTest
+    @EnumSource(value = ArmingStatus.class, names = {"ARMED_HOME", "ARMED_AWAY"})
+    @DisplayName("Test 10")
+    public void system_armed_test(ArmingStatus armingStatus) {
         sensorSet = new HashSet<>(Arrays.asList(sensor1, sensor2, sensor3));
         Mockito.when(securityService.getSensors()).thenReturn(sensorSet);
-        if (repetitionInfo.getCurrentRepetition() == 1) {
-            securityService.setArmingStatus(ArmingStatus.ARMED_HOME);
-        } else {
-            securityService.setArmingStatus(ArmingStatus.ARMED_AWAY);
-        }
+        Mockito.when(securityRepository.getArmingStatus()).thenReturn(ArmingStatus.DISARMED);
+        securityService.setArmingStatus(armingStatus);
         Mockito.verify(sensor1).setActive(false);
         Mockito.verify(sensor2).setActive(false);
         Mockito.verify(sensor3).setActive(false);
     }
 
-    @RepeatedTest(2)
-    public void armed_home_with_cat(RepetitionInfo repetitionInfo) {
-        if (repetitionInfo.getCurrentRepetition() == 1) {
-            securityService.setArmingStatus(ArmingStatus.DISARMED);
-        } else {
-            securityService.setArmingStatus(ArmingStatus.ARMED_AWAY);
-        }
+    @Test
+    public void armed_home_with_cat() {
+        Mockito.when(securityRepository.getArmingStatus()).thenReturn(ArmingStatus.ARMED_HOME);
         Mockito.when(imageService.imageContainsCat(bufferedImage, 50.0f)).thenReturn(true);
         securityService.processImage(bufferedImage);
         securityService.setArmingStatus(ArmingStatus.ARMED_HOME);
-        Assertions.assertEquals(AlarmStatus.ALARM, securityService.getAlarmStatus());
+        Mockito.verify(securityRepository, Mockito.times(2)).setAlarmStatus(AlarmStatus.ALARM);
     }
 }
 
