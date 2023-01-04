048c13eeaa5590bceebd02727d4b266cc86408c2
Yi Kuang <y24kuang@uwaterloo.ca>
2023-01-04 16:10:55 -0500

    1.9

--- SecurityServiceModule/src/test/java/com/udacity/security/service/SecurityServiceTest.java
+++ SecurityServiceModule/src/test/java/com/udacity/security/service/SecurityServiceTest.java
@@ -46,7 +46,7 @@ public class SecurityServiceTest {
     @ParameterizedTest
     @EnumSource(value = ArmingStatus.class, names = {"ARMED_HOME", "ARMED_AWAY"})
     @DisplayName("Test 1")
-    public void alarm_armed_and_sensors_activated_test(ArmingStatus armingStatus) {
+    public void alarmArmed_sensorsActivated_pendingAlarm(ArmingStatus armingStatus) {
         Mockito.when(sensor1.getActive()).thenReturn(false);
         Mockito.when(securityRepository.getArmingStatus()).thenReturn(armingStatus);
         Mockito.when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.NO_ALARM);
@@ -57,7 +57,7 @@ public class SecurityServiceTest {
     @ParameterizedTest
     @EnumSource(value = ArmingStatus.class, names = {"ARMED_HOME", "ARMED_AWAY"})
     @DisplayName("Test 2")
-    public void alarm_armed_and_sensor_activated_pending_alarm_test(ArmingStatus armingStatus) {
+    public void alarmArmed_sensorActivated_pendingAlarm_changeToAlarm(ArmingStatus armingStatus) {
         Mockito.when(sensor1.getActive()).thenReturn(false);
         Mockito.when(securityRepository.getArmingStatus()).thenReturn(armingStatus);
         Mockito.when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.PENDING_ALARM);
@@ -68,7 +68,7 @@ public class SecurityServiceTest {
 
     @Test
     @DisplayName("Test 3")
-    public void pending_alarm_inactive_sensors_test() {
+    public void pendingAlarm_sensorsInactive_returnToNoAlarm() {
         Mockito.when(sensor1.getActive()).thenReturn(true);
         Mockito.when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.PENDING_ALARM);
         securityService.changeSensorActivationStatus(sensor1, false);
@@ -77,7 +77,7 @@ public class SecurityServiceTest {
 
     @Test
     @DisplayName("Test 4")
-    public void active_alarm_changing_sensors_test() {
+    public void activeAlarm_changingSensorsStates_AlarmStateNotChanged() {
         Mockito.when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.ALARM);
         Mockito.when(sensor1.getActive()).thenReturn(true);
         Mockito.when(sensor2.getActive()).thenReturn(true);
@@ -93,7 +93,7 @@ public class SecurityServiceTest {
     @ParameterizedTest
     @EnumSource(value = ArmingStatus.class, names = {"ARMED_HOME", "ARMED_AWAY"})
     @DisplayName("Test 5")
-    public void one_sensor_active_another_being_activated_test(ArmingStatus armingStatus) {
+    public void oneSensorActive_anotherBeingActivated_alreadyPending_changeToAlarm(ArmingStatus armingStatus) {
         Mockito.when(securityRepository.getArmingStatus()).thenReturn(armingStatus);
         Mockito.when(securityRepository.getAlarmStatus()).thenReturn(AlarmStatus.NO_ALARM);
         Mockito.when(sensor1.getActive()).thenReturn(false);
@@ -107,7 +107,7 @@ public class SecurityServiceTest {
 
     @Test
     @DisplayName("Test 6")
-    public void sensor_deactivated_already_inactive_test() {
+    public void sensorDeactivated_alreadyInactive_noChangesToAlarmState() {
         Mockito.when(sensor1.getActive()).thenReturn(false);
         securityService.changeSensorActivationStatus(sensor1, false);
         Mockito.verify(securityRepository, Mockito.never()).setAlarmStatus(AlarmStatus.ALARM);
@@ -117,7 +117,7 @@ public class SecurityServiceTest {
 
     @Test
     @DisplayName("Test 7")
-    public void contains_cat_and_armed_home_test() {
+    public void containsCat_armedHome_changeToAlarmStatus() {
         Mockito.when(securityRepository.getArmingStatus()).thenReturn(ArmingStatus.ARMED_HOME);
         Mockito.when(imageService.imageContainsCat(bufferedImage, 50.0f)).thenReturn(true);
         securityService.processImage(bufferedImage);
@@ -126,7 +126,7 @@ public class SecurityServiceTest {
 
     @RepeatedTest(2)
     @DisplayName("Test 8")
-    public void does_not_contains_cat_test(RepetitionInfo repetitionInfo) {
+    public void doesNotContainsCat_sensorsNotActive_changeToNoAlarm(RepetitionInfo repetitionInfo) {
         Mockito.when(imageService.imageContainsCat(bufferedImage, 50.0f)).thenReturn(false);
         Mockito.when(sensor1.getActive()).thenReturn(false);
         Mockito.when(sensor2.getActive()).thenReturn(false);
@@ -149,7 +149,7 @@ public class SecurityServiceTest {
 
     @Test
     @DisplayName("Test 9")
-    public void system_disarmed_test() {
+    public void systemDisarmed_changeToNoAlarm() {
         securityService.setArmingStatus(ArmingStatus.DISARMED);
         Mockito.verify(securityRepository).setAlarmStatus(AlarmStatus.NO_ALARM);
     }
@@ -157,7 +157,7 @@ public class SecurityServiceTest {
     @ParameterizedTest
     @EnumSource(value = ArmingStatus.class, names = {"ARMED_HOME", "ARMED_AWAY"})
     @DisplayName("Test 10")
-    public void system_armed_test(ArmingStatus armingStatus) {
+    public void systemArmed_resetAllSensorsToInactive(ArmingStatus armingStatus) {
         sensorSet = new HashSet<>(Arrays.asList(sensor1, sensor2, sensor3));
         Mockito.when(securityService.getSensors()).thenReturn(sensorSet);
         Mockito.when(securityRepository.getArmingStatus()).thenReturn(ArmingStatus.DISARMED);
@@ -168,7 +168,8 @@ public class SecurityServiceTest {
     }
 
     @Test
-    public void armed_home_with_cat() {
+    @DisplayName("Test 11")
+    public void armedHomeWithCat_changeToAlarm() {
         Mockito.when(securityRepository.getArmingStatus()).thenReturn(ArmingStatus.ARMED_HOME);
         Mockito.when(imageService.imageContainsCat(bufferedImage, 50.0f)).thenReturn(true);
         securityService.processImage(bufferedImage);
